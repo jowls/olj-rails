8a7ce998226b8d4beb9f5af9fc22a33a64f4ed62
Joseph Allan <joseph.s.allan@gmail.com>
2013-11-03 19:49:00 -0400

    Removing app wide token authentication. Was probably a mistake to add that before filter on app controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   # This is our new function that comes before Devise's one
-  before_filter :authenticate_user_from_token!
+  #before_filter :authenticate_user_from_token!
   # This is Devise's authentication
   before_filter :authenticate_user!
 
