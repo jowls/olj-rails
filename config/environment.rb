1f5fe58131a6c31830879bf5841cc25ce17c5176
Joseph Allan <joseph.s.allan@gmail.com>
2014-01-17 17:26:55 -0400

    Setting up actionmailer for devise to use olj.org smtp settings.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Oljournal::Application.initialize!
+
+LandingPage::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      :address   => "smtp.onelinejournal.org",
+      :port      => 587, # or 25
+      :enable_starttls_auto => true, # detects and uses STARTTLS
+      :user_name => "admin@onelinejournal.org",
+      :password  => "~~~REMOVED~~~",
+      :authentication => 'login'
+  }
