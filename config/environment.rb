bbdfa97c3ce3dd679e5c2eca72066a88225cd35e
Joseph Allan <joseph.s.allan@gmail.com>
2014-01-17 22:14:18 -0400

    Email password reset working.

--- config/environment.rb
+++ config/environment.rb
@@ -8,11 +8,11 @@ Oljournal::Application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-      :address   => "smtp.onelinejournal.org",
-      :port      => 587, # or 25
-      :enable_starttls_auto => true, # detects and uses STARTTLS
-      :user_name => "admin@onelinejournal.org",
-      :password  => "~~~REMOVED~~~",
+      :openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE,
+      :address   => 'mail.onelinejournal.org',
+      :port      => 2525, # or 25
+      :user_name => 'admin@onelinejournal.org',
+      :password  => 'k4ye2rcp',
       :authentication => 'login'
   }
 end
